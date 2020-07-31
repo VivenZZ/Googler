de2e46e7066d4e0a0fba78957aef7e0168d46d93
Viven <17706500512@163.com>
2020-07-31 17:51:01 +0800

    2020/07/31

--- code/1.js
+++ code/1.js
@@ -1,20 +1,18 @@
-
-
-var getName
-function getName() { console.log (5);}
 function Foo() {
   getName = function () { console.log (1); };
   return this;
 }
-Foo.getName = function () { console.log (2);};
+Foo.getName = function () {
+  console.log (2);
+};
 Foo.prototype.getName = function () { console.log (3);};
-getName = function () { console.log (4);};
-
+var getName = function () { console.log (4);};
+function getName() { console.log (5);}
 //请写出以下输出结果：
-Foo.getName(); // 2
-getName(); // 4
-Foo().getName(); // 2
-getName(); // 4
-new Foo.getName(); //2
-new Foo().getName(); //3
-new new Foo().getName(); //3
+Foo.getName();
+getName();
+Foo().getName();
+getName();
+new Foo.getName();
+new Foo().getName();
+new new Foo().getName();
--- code/2.js
+++ code/2.js
@@ -1,1 +1,4 @@
-const a  = 1
+
+let arr1 = [1,3,11,5,7,41]
+arr1.sort()
+console.log(arr1)
