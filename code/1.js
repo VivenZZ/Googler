61c8975a4d8f8a2ac668afe4293ed42ff03e62e7
VivenZ <17706500512@163.com>
2020-08-02 00:37:49 +0800

    函数 以及基本包装类型

--- code/1.js
+++ code/1.js
@@ -1,15 +1,4 @@
 
-let arr = [1,2,3,4,5,6,7]
-// 删除第二项
-console.log(arr.splice(1,1)) // [2]
-console.log(arr) // [1,3,4,5,6,7]
-// 修改第二项
-console.log(arr.splice(1,1,8)) // [3]
-console.log(arr) // [1,8,4,5,6,7]
-// 删除8，4，新增9，10，11
-console.log(arr.splice(1,2,9,10,11)) // [8,4]
-console.log(arr) // [1,9,10,11,5,6,7]
-
-// 新增
-console.log(arr.splice(1,0,12)) // []
-console.log(arr) // [1,12, 9,10,11,5,6,7]
+  let str = 'hello'
+  let str1 = str.concat(' world', '!')
+  console.log(str1)
