05890c5a9595a0f661c9811c7fffb202a0bed7f2
VivenZ <17706500512@163.com>
2020-08-02 17:27:11 +0800

    函数

--- code/1.js
+++ code/1.js
@@ -1,4 +1,19 @@
 
-  let str = 'hello'
-  let str1 = str.concat(' world', '!')
-  console.log(str1)
+function Person(name, age) {
+  this.name = name
+  this.age = age
+}
+Person.prototype.job = 'doctor'
+let p1 = new Person('viven', 23)
+// 判断是否是原型上的属性
+function hasPrototypeProperty(object, name){
+  return !object.hasOwnProperty(name) &&  (name in object)
+}
+console.log(hasPrototypeProperty(p1, 'name')); // false
+console.log(hasPrototypeProperty(p1, 'job')); // true
+
+for(let key in p1) {
+  console.log(key)
+}
+let keys =Object.keys(p1)
+console.log(keys); // [ 'name', 'age' ]
--- note/vscode配置.md
+++ note/vscode配置.md
@@ -1,3 +1,7 @@
+# vscode配置
+## vscode js文件调试
+在launch.json中添加一下代码
+```json
 {
   "version": "0.2.0",
   "configurations": [
@@ -18,3 +22,5 @@
     }
   ]
 }
+```
+按F5进入调试，选择Launch Program 进入debugger模式
