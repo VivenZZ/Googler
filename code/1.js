e247a34085a8f79cc28a31e8670edfbc83b2b745
VivenZ <17706500512@163.com>
2020-08-02 23:37:27 +0800

    对象原型

--- code/1.js
+++ code/1.js
@@ -2,18 +2,13 @@
 function Person(name, age) {
   this.name = name
   this.age = age
+  Person.prototype.getName = function() {
+    console.log(this.name)
+  }
 }
-Person.prototype.job = 'doctor'
+  
 let p1 = new Person('viven', 23)
-// 判断是否是原型上的属性
-function hasPrototypeProperty(object, name){
-  return !object.hasOwnProperty(name) &&  (name in object)
-}
-console.log(hasPrototypeProperty(p1, 'name')); // false
-console.log(hasPrototypeProperty(p1, 'job')); // true
-
-for(let key in p1) {
-  console.log(key)
-}
-let keys =Object.keys(p1)
-console.log(keys); // [ 'name', 'age' ]
+let p2 = new Person('kevin', 23)
+p1.getName()
+p2.getName()
+console.log(p1.getName === p2.getName);
