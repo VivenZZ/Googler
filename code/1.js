4068efeb240f7fbcbca44eae437992353ca3d658
VivenZ <17706500512@163.com>
2020-08-04 00:06:01 +0800

    继承

--- code/1.js
+++ code/1.js
@@ -1,14 +1,22 @@
 
-function Person(name, age) {
+function A(name){
   this.name = name
-  this.age = age
-  Person.prototype.getName = function() {
-    console.log(this.name)
-  }
+  this.colors = ['red','green','yellow']
 }
-  
-let p1 = new Person('viven', 23)
-let p2 = new Person('kevin', 23)
-p1.getName()
-p2.getName()
-console.log(p1.getName === p2.getName);
+function B(name, age){
+  this.age = age
+  A.call(this, name)
+}
+A.prototype.getName = function(){
+  console.log(this.name)
+}
+// B的原型需要的是A的原型，所以我们先创建一个A的原型的副本，添加给B就可以了
+B.prototype = Object.create(A.prototype)
+B.prototype.constructor = B
+let b1 = new B('viven', 23)
+let b2 = new B('kevin', 26)
+b1.colors.push('black')
+console.log(b1.name,b1.age,b1.colors.toString()) // viven 23 red,green,yellow,black
+console.log(b2.name,b2.age,b2.colors.toString()) // kevin 26 red,green,yellow
+b1.getName()
+b2.getName()
